9f024a70084f65767e796b1767a1dce8cc744ea1
qfu <qfu@ucsd.edu>
2017-03-05 15:41:38 -0800

    some update

--- frequentPhraseMine.py
+++ frequentPhraseMine.py
@@ -12,36 +12,38 @@ from nltk.tokenize import word_tokenize
 tokenizer = RegexpTokenizer(r'\w+')
 #tokenizer = RegexpTokenizer('[A-Z]\w+')
 class TweetPreProcess:
-    emoticons_str = r"""
-        (?:
-            [:=;] # Eyes
-            [oO\-]? # Nose (optional)
-            [D\)\]\(\]/\\OpP] # Mouth
-        )"""
+    def __init__(self):
+        self.emoticons_str = r"""
+            (?:
+                [:=;] # Eyes
+                [oO\-]? # Nose (optional)
+                [D\)\]\(\]/\\OpP] # Mouth
+            )"""
 
-    regex_str = [
-        emoticons_str,
-        r'<[^>]+>', # HTML tags
-        r'(?:@[\w_]+)', # @-mentions
-        r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
-        r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+        self.regex_str = [
+            self.emoticons_str,
+            r'<[^>]+>', # HTML tags
+            r'(?:@[\w_]+)', # @-mentions
+            r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+            r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
 
-        r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
-        r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
-        r'(?:[\w_]+)', # other words
-        r'(?:\S)' # anything else
-    ]
+            r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+            r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+            r'(?:[\w_]+)', # other words
+            r'(?:\S)' # anything else
+        ]
 
-    tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
-    emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+        self.tokens_re = re.compile(r'('+'|'.join(self.regex_str)+')', re.VERBOSE | re.IGNORECASE)
+        self.emoticon_re = re.compile(r'^'+self.emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
 
-    def tokenize(s):
-        return tokens_re.findall(s)
 
-    def preprocess(s, lowercase=False):
-        tokens = tokenize(s)
+    def tokenize(self,s):
+        return self.tokens_re.findall(s)
+
+    def preprocess(self,s, lowercase=False):
+        tokens = self.tokenize(s)
         if lowercase:
-            tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+            tokens = [token if self.emoticon_re.search(token) else token.lower() for token in tokens]
         return tokens
 
 
@@ -110,7 +112,9 @@ class Utility:
     def loadJson(line):
         formatStr = ""
         tweet = json.loads(line).get('tweet')
+        tpp = TweetPreProcess()
         if (tweet.get('text') != None and tweet.get('lang') == 'en'):
+            #formatStr = " ".join(tpp.preprocess(tweet.get('text'),True)) + ".";
             formatStr = " ".join(tokenizer.tokenize(tweet.get('text'))) + ".";
         return formatStr;
     @staticmethod
